d60442a57431417808da5316c6930eedd148f3b3
Georgi Kalchev <g.kalchev93@gmail.com>
2021-03-18 19:43:30 +0200

    Initialize solutions projects

---
+++ DevOcean.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DevOcean.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
