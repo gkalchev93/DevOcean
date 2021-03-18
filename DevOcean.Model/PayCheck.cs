c8db1c447a0bccf790f0ef87750df13c99665a34
Georgi Kalchev <g.kalchev93@gmail.com>
2021-03-18 19:50:51 +0200

    Add PayCheck Model

---
+++ DevOcean.Model/PayCheck.cs
@@ -0,0 +1,17 @@
+﻿namespace DevOcean.Model
+{
+    public class PayCheck
+    {
+        public double GrossSalary { get; set; }
+        public double IncomeTax { get; set; }
+        public double ContributionTax { get; set; }
+
+        public double NetSalary
+        {
+            get
+            {
+                return GrossSalary - IncomeTax - ContributionTax;
+            }
+        }
+    }
+}
